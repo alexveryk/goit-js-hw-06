60e936ee6fa546373d5a7b3fb0dfd2169490e327
alexveryk <veryk.av@gmail.com>
2022-12-05 02:24:22 +0200

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const input = document.querySelector("#validation-input");
+
+input.addEventListener("blur", () => {
+  if (input.value.length < input.dataset.length) {
+    input.classList.add("invalid");
+  }
+  if (input.value.length >= input.dataset.length) {
+    input.classList.add("valid");
+  }
+
+  if (input.value.length >= input.dataset.length) {
+    input.classList.replace("invalid", "valid");
+  }
+});
