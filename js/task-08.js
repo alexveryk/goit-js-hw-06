2c4656a226a688bc4f0a070eb3205ef4daa6f3e5
alexveryk <veryk.av@gmail.com>
2022-12-05 02:24:26 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", submitForm);
 
+function submitForm(event) {
+  event.preventDefault();
+
+  const { email, password } = loginForm.elements;
+  if (!email.value || !password.value) {
+    return alert("Всі поля повинні бути заповнені");
+  }
+  const userData = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(userData);
+  event.currentTarget.reset();
+}
