999ed51033ba607c89f5d7c770bc776279269827
alexveryk <veryk.av@gmail.com>
2022-12-05 02:24:12 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+let ingredientsItem = [];
+const list = document.querySelector("#ingredients");
+ingredients.forEach((element) => {
+  const li = document.createElement("li");
+  li.classList.add("item");
+  li.textContent = element;
+  ingredientsItem.push(li);
+});
+
+list.append(...ingredientsItem);
