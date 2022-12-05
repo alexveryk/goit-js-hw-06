088811db81bcd7e33da3f617a493beb3aa1ab47a
alexveryk <veryk.av@gmail.com>
2022-12-05 02:24:28 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const changeColor = document.querySelector(".change-color");
+const backgrounColor = document.querySelector(".color");
+
+changeColor.addEventListener("click", () => {
+  let color = getRandomHexColor();
+  backgrounColor.textContent = color;
+  document.body.style.backgroundColor = color;
+});
